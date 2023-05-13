37b07e74695e8b54077ac2a7314a08f4ba56902a
Lautaro <lautiquiroga10@gmail.com>
2023-05-12 22:29:48 -0300

    proyectos y navbar position:fixed

--- client/src/components/Proyectos.jsx
+++ client/src/components/Proyectos.jsx
@@ -3,5 +3,36 @@
 import React from "react";
 
 export default function Proyectos() {
-  return <div className="proyectosContainer"></div>;
+  return (
+    <div className="proyectosContainer">
+      <div className="titulo-proyectos">
+        Descubre más sobre <br /> nuestros trabajos
+      </div>
+      <div className="descr-proyectos">
+        Aquí podrás ver y elegir entre todos nuestros trabajos realizados.
+      </div>
+      <div className="cards">
+        <div className="cards-flex">
+          <a
+            href="https://san-tor.vercel.app/index.html"
+            className="card"
+            target="_blank"
+          ></a>
+          <a
+            href="https://san-tor.vercel.app/index.html"
+            className="card"
+            target="_blank"
+          ></a>
+          <a
+            href="https://san-tor.vercel.app/index.html"
+            className="card"
+            target="_blank"
+          ></a>
+        </div>
+        {/* <a className="button-proyectos" href="">
+          VER MÁS
+        </a> */}
+      </div>
+    </div>
+  );
 }
