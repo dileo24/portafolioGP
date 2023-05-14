16bb9958dbb488b563273d007c6e02b50228144f
Joaquin <joacodileo@gmail.com>
2023-05-14 12:19:34 -0300

    creando planes

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,6 +4,7 @@ import Footer from "./Footer";
 import ButtonExt from "./ButtonExt";
 import Nosotros from "./Nosotros";
 import Proyectos from "./Proyectos";
+import Planes from "./Planes";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         <Nosotros />
         <ButtonExt />
         <Proyectos />
+        <Planes />
       </div>
       <Footer />
     </div>
--- client/src/components/Planes.jsx
+++ client/src/components/Planes.jsx
@@ -3,5 +3,68 @@
 import React from "react";
 
 export default function Planes() {
-  return <div className="planesContainer"></div>;
+  return (
+    <div className="planesContainer">
+      <div className="planesYPrecios">
+        <p>Planes y Precios</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio eos,
+          magni fugit iusto possimus vero, odit esse consequatur incidunt
+          assumenda reiciendis voluptatum. Itaque totam accusamus vero ea soluta
+          modi incidunt?
+        </p>
+      </div>
+      <div className="plan">
+        <p>Bronce</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quod
+          maiores saepe doloremque sequi dicta? Quod sunt, a odio aliquam ipsa
+          nulla deserunt quibusdam, rem cupiditate porro iste accusamus
+          laboriosam!
+        </p>
+        <div className="detalles">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            quod maiores saepe doloremque sequi dicta? Quod sunt, a odio aliquam
+            ipsa nulla deserunt quibusdam, rem cupiditate porro iste accusamus
+            laboriosam!
+          </p>
+        </div>
+      </div>
+      <div className="plan">
+        <p>Oro</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quod
+          maiores saepe doloremque sequi dicta? Quod sunt, a odio aliquam ipsa
+          nulla deserunt quibusdam, rem cupiditate porro iste accusamus
+          laboriosam!
+        </p>
+        <div className="detalles">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            quod maiores saepe doloremque sequi dicta? Quod sunt, a odio aliquam
+            ipsa nulla deserunt quibusdam, rem cupiditate porro iste accusamus
+            laboriosam!
+          </p>
+        </div>
+      </div>
+      <div className="plan">
+        <p>Plata</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quod
+          maiores saepe doloremque sequi dicta? Quod sunt, a odio aliquam ipsa
+          nulla deserunt quibusdam, rem cupiditate porro iste accusamus
+          laboriosam!
+        </p>
+        <div className="detalles">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            quod maiores saepe doloremque sequi dicta? Quod sunt, a odio aliquam
+            ipsa nulla deserunt quibusdam, rem cupiditate porro iste accusamus
+            laboriosam!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
