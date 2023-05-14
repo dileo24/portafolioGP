28d45624d2d00b0580c7d775d51a40c07e4259b3
Joaquin <joacodileo@gmail.com>
2023-05-14 13:43:06 -0300

    haciendo cuadros de planes

--- client/src/components/Planes.jsx
+++ client/src/components/Planes.jsx
@@ -15,55 +15,58 @@ export default function Planes() {
           </p>
         </div>
       </div>
-      <div className="plan">
-        <p>Bronce</p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quod
-          maiores saepe doloremque sequi dicta? Quod sunt, a odio aliquam ipsa
-          nulla deserunt quibusdam, rem cupiditate porro iste accusamus
-          laboriosam!
-        </p>
-        <div className="detalles">
+
+      <div className="planes">
+        <div className="plan">
+          <p>Bronce</p>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
             quod maiores saepe doloremque sequi dicta? Quod sunt, a odio aliquam
             ipsa nulla deserunt quibusdam, rem cupiditate porro iste accusamus
             laboriosam!
           </p>
+          <div className="detalles">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+              quod maiores saepe doloremque sequi dicta? Quod sunt, a odio
+              aliquam ipsa nulla deserunt quibusdam, rem cupiditate porro iste
+              accusamus laboriosam!
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="plan">
-        <p>Oro</p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quod
-          maiores saepe doloremque sequi dicta? Quod sunt, a odio aliquam ipsa
-          nulla deserunt quibusdam, rem cupiditate porro iste accusamus
-          laboriosam!
-        </p>
-        <div className="detalles">
+        <div className="plan">
+          <p>Oro</p>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
             quod maiores saepe doloremque sequi dicta? Quod sunt, a odio aliquam
             ipsa nulla deserunt quibusdam, rem cupiditate porro iste accusamus
             laboriosam!
           </p>
+          <div className="detalles">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+              quod maiores saepe doloremque sequi dicta? Quod sunt, a odio
+              aliquam ipsa nulla deserunt quibusdam, rem cupiditate porro iste
+              accusamus laboriosam!
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="plan">
-        <p>Plata</p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quod
-          maiores saepe doloremque sequi dicta? Quod sunt, a odio aliquam ipsa
-          nulla deserunt quibusdam, rem cupiditate porro iste accusamus
-          laboriosam!
-        </p>
-        <div className="detalles">
+        <div className="plan">
+          <p>Plata</p>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
             quod maiores saepe doloremque sequi dicta? Quod sunt, a odio aliquam
             ipsa nulla deserunt quibusdam, rem cupiditate porro iste accusamus
             laboriosam!
           </p>
+          <div className="detalles">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+              quod maiores saepe doloremque sequi dicta? Quod sunt, a odio
+              aliquam ipsa nulla deserunt quibusdam, rem cupiditate porro iste
+              accusamus laboriosam!
+            </p>
+          </div>
         </div>
       </div>
     </div>
