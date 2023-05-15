93f1682cfaba3bac08b68850b57dc96b30d913cb
Lautaro <lautiquiroga10@gmail.com>
2023-05-15 20:15:29 -0300

    arreglo planes

--- client/src/components/Planes.jsx
+++ client/src/components/Planes.jsx
@@ -64,29 +64,39 @@ export default function Planes() {
             venta y podrá usted mismo gestionar cada dato!
           </p>
           <div className="detalles">
-            <p className="text">
-              Diseño tipo landing page o multipágina.
-              <br />
-              <br />
-              Hasta 8 secciones.
-              <br />
-              <br />
-              Vista responsive (adaptable a celulares, tablets, computadora).
-              <br />
-              <br />
-              Formularios ilimitados (contacto, cotizacion, etc).
-              <br />
-              <br />
-              Tema claro y oscuro.
-              <br />
-              <br />
-              Carga de información mas abundante (estilo Ecommerce por ejemplo).
-              <br />
-              <br />
-              Autogestionable y sistema de registro/inicio de sesión.
-              <br />
-              <br />
-            </p>
+            <div className="lista">
+              <p>
+                <img src={check} alt="check" className="check" />
+                Diseño tipo landing page o multipágina.
+                <br />
+                <br />
+                <img src={check} alt="check" className="check" />
+                Hasta 8 secciones.
+                <br />
+                <br />
+                <img src={check} alt="check" className="check" />
+                Vista responsive (adaptable a celulares, tablets, computadora).
+                <br />
+                <br />
+                <img src={check} alt="check" className="check" />
+                Formularios ilimitados (contacto, cotizacion, etc).
+                <br />
+                <br />
+                <img src={check} alt="check" className="check" />
+                Tema claro y oscuro.
+                <br />
+                <br />
+                <img src={check} alt="check" className="check" />
+                Carga de información mas abundante (estilo Ecommerce por
+                ejemplo).
+                <br />
+                <br />
+                <img src={check} alt="check" className="check" />
+                Autogestionable y sistema de registro/inicio de sesión.
+                <br />
+                <br />
+              </p>
+            </div>
             <div className="precio">
               <div className="prec">
                 <p className="moneda"> AR$</p>
