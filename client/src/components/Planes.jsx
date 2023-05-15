489662d05b5d9214db458188ea7d446074fb9e81
Joaquin <joacodileo@gmail.com>
2023-05-15 13:10:26 -0300

    cards de planes

--- client/src/components/Planes.jsx
+++ client/src/components/Planes.jsx
@@ -18,54 +18,103 @@ export default function Planes() {
 
       <div className="planes">
         <div className="plan">
-          <p>Bronce</p>
+          <p className="nombrePlan">BRONCE</p>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-            quod maiores saepe doloremque sequi dicta? Quod sunt, a odio aliquam
-            ipsa nulla deserunt quibusdam, rem cupiditate porro iste accusamus
-            laboriosam!
+            quod maiores saepe doloremque sequi dicta?
           </p>
           <div className="detalles">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-              quod maiores saepe doloremque sequi dicta? Quod sunt, a odio
-              aliquam ipsa nulla deserunt quibusdam, rem cupiditate porro iste
-              accusamus laboriosam!
+              -Diseño tipo landing page o multipágina.
+              <br />
+              <br />
+              -Hasta 4 secciones.
+              <br />
+              <br />
+              -Vista responsive(adaptable a celulares, tablets, computadora).
+              <br />
+              <br />
+              -Formulario de contacto.
+              <br />
+              <br />
             </p>
+            <div className="precio">
+              <p className="precioNum">$40.000 (ARS)</p>
+              <p className="aclaracion">(Pago único)</p>
+            </div>
           </div>
         </div>
         <div className="plan">
-          <p>Oro</p>
+          <p className="nombrePlan">ORO</p>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-            quod maiores saepe doloremque sequi dicta? Quod sunt, a odio aliquam
-            ipsa nulla deserunt quibusdam, rem cupiditate porro iste accusamus
-            laboriosam!
+            quod maiores saepe doloremque sequi dicta?
           </p>
           <div className="detalles">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-              quod maiores saepe doloremque sequi dicta? Quod sunt, a odio
-              aliquam ipsa nulla deserunt quibusdam, rem cupiditate porro iste
-              accusamus laboriosam!
+              -Diseño tipo landing page o multipágina.
+              <br />
+              <br />
+              -Hasta 8 secciones.
+              <br />
+              <br />
+              -Vista responsive(adaptable a celulares, tablets, computadora).
+              <br />
+              <br />
+              -Formularios ilimitados (contacto, cotizacion, etc).
+              <br />
+              <br />
+              -Tema claro y oscuro.
+              <br />
+              <br />
+              -Carga de información mas abundante (estilo Ecommerce por
+              ejemplo).
+              <br />
+              <br />
+              -Autogestionable por los administradores de su empresa (creacion
+              de productos, eliminación de productos, etc).
+              <br />
+              <br />
+              -Sistema de registro/inicio de sesión.
+              <br />
+              <br />
             </p>
+            <div className="precio">
+              <p className="precioNum">$100.000 (ARS)</p>
+              <p className="aclaracion">(Pago único)</p>
+            </div>
           </div>
         </div>
         <div className="plan">
-          <p>Plata</p>
+          <p className="nombrePlan">PLATA</p>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-            quod maiores saepe doloremque sequi dicta? Quod sunt, a odio aliquam
-            ipsa nulla deserunt quibusdam, rem cupiditate porro iste accusamus
-            laboriosam!
+            -Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            quod maiores saepe doloremque sequi dicta?
           </p>
           <div className="detalles">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-              quod maiores saepe doloremque sequi dicta? Quod sunt, a odio
-              aliquam ipsa nulla deserunt quibusdam, rem cupiditate porro iste
-              accusamus laboriosam!
+              -Diseño tipo landing page o multipágina.
+              <br />
+              <br />
+              -Hasta 6 secciones.
+              <br />
+              <br />
+              -Vista responsive(adaptable a celulares, tablets, computadora).
+              <br />
+              <br />
+              -Hasta 3 formularios (contacto, cotizacion, etc).
+              <br />
+              <br />
+              -Tema claro y oscuro.
+              <br />
+              <br />
+              -Carga de información mas abundante (estilo Ecommerce por
+              ejemplo).
             </p>
+            <div className="precio">
+              <p className="precioNum">$60.000 (ARS)</p>
+              <p className="aclaracion">(Pago único)</p>
+            </div>
           </div>
         </div>
       </div>
