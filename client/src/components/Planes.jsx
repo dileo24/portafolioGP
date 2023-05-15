ca9e1b650285d3fa9d5c244d3d5d46a578c615d5
Joaquin <joacodileo@gmail.com>
2023-05-15 14:26:06 -0300

    arreglando cards de planes

--- client/src/components/Planes.jsx
+++ client/src/components/Planes.jsx
@@ -19,103 +19,109 @@ export default function Planes() {
       <div className="planes">
         <div className="plan">
           <p className="nombrePlan">BRONCE</p>
-          <p>
+          <p className="text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
             quod maiores saepe doloremque sequi dicta?
           </p>
           <div className="detalles">
-            <p>
-              -Diseño tipo landing page o multipágina.
+            <p className="text">
+              Diseño tipo landing page o multipágina.
               <br />
               <br />
-              -Hasta 4 secciones.
+              Hasta 4 secciones.
               <br />
               <br />
-              -Vista responsive(adaptable a celulares, tablets, computadora).
+              Vista responsive (adaptable a celulares, tablets, computadora).
               <br />
               <br />
-              -Formulario de contacto.
+              Formulario de contacto.
               <br />
               <br />
             </p>
             <div className="precio">
-              <p className="precioNum">$40.000 (ARS)</p>
+              <div className="prec">
+                <p className="moneda"> AR$</p>
+                <p className="precioNum">40.000 </p>
+              </div>
               <p className="aclaracion">(Pago único)</p>
             </div>
           </div>
+          <button>LO QUIERO</button>
         </div>
         <div className="plan">
           <p className="nombrePlan">ORO</p>
-          <p>
+          <p className="text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
             quod maiores saepe doloremque sequi dicta?
           </p>
           <div className="detalles">
-            <p>
-              -Diseño tipo landing page o multipágina.
+            <p className="text">
+              Diseño tipo landing page o multipágina.
               <br />
               <br />
-              -Hasta 8 secciones.
+              Hasta 8 secciones.
               <br />
               <br />
-              -Vista responsive(adaptable a celulares, tablets, computadora).
+              Vista responsive (adaptable a celulares, tablets, computadora).
               <br />
               <br />
-              -Formularios ilimitados (contacto, cotizacion, etc).
+              Formularios ilimitados (contacto, cotizacion, etc).
               <br />
               <br />
-              -Tema claro y oscuro.
+              Tema claro y oscuro.
               <br />
               <br />
-              -Carga de información mas abundante (estilo Ecommerce por
-              ejemplo).
+              Carga de información mas abundante (estilo Ecommerce por ejemplo).
               <br />
               <br />
-              -Autogestionable por los administradores de su empresa (creacion
-              de productos, eliminación de productos, etc).
-              <br />
-              <br />
-              -Sistema de registro/inicio de sesión.
+              Autogestionable (y sistema de registro/inicio de sesión).
               <br />
               <br />
             </p>
             <div className="precio">
-              <p className="precioNum">$100.000 (ARS)</p>
+              <div className="prec">
+                <p className="moneda"> AR$</p>
+                <p className="precioNum">40.000 </p>
+              </div>
               <p className="aclaracion">(Pago único)</p>
             </div>
           </div>
+          <button>LO QUIERO</button>
         </div>
         <div className="plan">
           <p className="nombrePlan">PLATA</p>
-          <p>
-            -Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+          <p className="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
             quod maiores saepe doloremque sequi dicta?
           </p>
           <div className="detalles">
-            <p>
-              -Diseño tipo landing page o multipágina.
+            <p className="text">
+              Diseño tipo landing page o multipágina.
               <br />
               <br />
-              -Hasta 6 secciones.
+              Hasta 6 secciones.
               <br />
               <br />
-              -Vista responsive(adaptable a celulares, tablets, computadora).
+              Vista responsive (adaptable a celulares, tablets, computadora).
               <br />
               <br />
-              -Hasta 3 formularios (contacto, cotizacion, etc).
+              Hasta 3 formularios (contacto, cotizacion, etc).
               <br />
               <br />
-              -Tema claro y oscuro.
+              Tema claro y oscuro.
               <br />
               <br />
-              -Carga de información mas abundante (estilo Ecommerce por
-              ejemplo).
+              Carga de información mas abundante (estilo Ecommerce por ejemplo).
             </p>
             <div className="precio">
-              <p className="precioNum">$60.000 (ARS)</p>
+              <div className="prec">
+                <p className="moneda"> AR$</p>
+                <p className="precioNum">40.000 </p>
+              </div>
               <p className="aclaracion">(Pago único)</p>
             </div>
           </div>
+          <button>LO QUIERO</button>
         </div>
       </div>
     </section>
