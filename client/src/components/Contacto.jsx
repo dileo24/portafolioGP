ea2fae8dc5bcafd654a0bab21e95b54e94e11e0a
Joaquin <joacodileo@gmail.com>
2023-05-17 15:40:06 -0300

    notificacion de contacto CON ERRORES

--- client/src/components/Contacto.jsx
+++ client/src/components/Contacto.jsx
@@ -1,19 +1,82 @@
 // Joaco
 
-import React from "react";
-/* import persona from "../multimedia/persona.png"; */
+import React, { useState } from "react";
+import axios from "axios";
+import { useSpring, animated } from "react-spring";
 import persona from "../multimedia/persona2.png";
+import enviadoCorr from "../multimedia/enviadoCorr.png";
 
 export default function Contacto() {
+  const [enviado, setEnviado] = useState(false);
+  const [animProps, setAnimProps] = useSpring(() => ({
+    opacity: 0,
+    transform: "scale(0.5)",
+  }));
+  const [input, setInput] = useState({
+    name: "",
+    empresa: "",
+    pais: "",
+    ciudad: "",
+    celular: "",
+    email: "",
+    mensaje: "",
+  });
+
+  const handlerChange = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setEnviado(true);
+
+    const formData = new FormData();
+    Object.entries(input).forEach(([id, value]) => {
+      formData.append(id, value);
+    });
+    formData.append("_subject", "Go Page Portafolio");
+    formData.append("_captcha", "false");
+
+    try {
+      const response = await axios.post(
+        "https://formsubmit.co/ajax/gopagearg@gmail.com",
+        formData
+      );
+      if (response.data.success) {
+        console.log("Se envió correctamente");
+        setAnimProps({
+          opacity: 1,
+          transform: "scale(1)",
+          position: "absolute",
+          display: "flex",
+        });
+        setTimeout(() => {
+          setAnimProps({ opacity: 0, transform: "scale(0.5)" });
+        }, 2000);
+      } else {
+        console.log(`Hubo un error: ${response.data}`);
+      }
+    } catch (error) {
+      console.log(`Hubo un error: ${error}`);
+    }
+    setTimeout(() => {
+      setEnviado(false);
+      setInput({
+        name: "",
+        empresa: "",
+        pais: "",
+        ciudad: "",
+        celular: "",
+        email: "",
+        mensaje: "",
+      });
+    }, 500);
+  };
+
   return (
     <section className="contactoContainer" id="contacto">
       <div className="formContainer">
-        <form
-          action="https://formsubmit.co/gopagearg@gmail.com"
-          method="POST"
-          name="contacto-santor"
-          netlify="true"
-        >
+        <form name="contacto-GoPage" netlify="true" onSubmit={handleSubmit}>
           <div className="text">
             <h1 className="titulo">Hablanos un poco más de vos...</h1>
             <p className="parrafo">
@@ -22,13 +85,6 @@ export default function Contacto() {
               poco de qué tenés pensado para tu proyecto. ¡Te esperamos!
             </p>
           </div>
-          <input type="hidden" name="_subject" value="Go Page Portafolio" />
-          <input type="hidden" name="_captcha" value="false" />
-          <input
-            type="hidden"
-            name="_next"
-            value="https://gopage.vercel.app/#contacto"
-          />
           <div className="inputS">
             <div className="inputSContainer">
               <div className="sect sect1">
@@ -36,6 +92,8 @@ export default function Contacto() {
                   type="text"
                   id="name"
                   name="name"
+                  value={input.name}
+                  onChange={(e) => handlerChange(e)}
                   required
                   placeholder="Nombre y apellido"
                 />
@@ -43,6 +101,8 @@ export default function Contacto() {
                   type="text"
                   id="empresa"
                   name="empresa"
+                  value={input.empresa}
+                  onChange={(e) => handlerChange(e)}
                   required
                   placeholder="Nombre de empresa"
                 />
@@ -50,6 +110,8 @@ export default function Contacto() {
                   type="text"
                   id="pais"
                   name="pais"
+                  value={input.pais}
+                  onChange={(e) => handlerChange(e)}
                   required
                   placeholder="País"
                 />
@@ -59,7 +121,9 @@ export default function Contacto() {
                 <input
                   type="text"
                   id="ciud"
-                  name="ciud"
+                  name="ciudad"
+                  value={input.ciudad}
+                  onChange={(e) => handlerChange(e)}
                   required
                   placeholder="Ciudad"
                 />
@@ -67,8 +131,10 @@ export default function Contacto() {
                 <input
                   type="number"
                   id="cel"
-                  name="cel"
+                  name="celular"
                   className="celIn"
+                  value={input.celular}
+                  onChange={(e) => handlerChange(e)}
                   required
                   placeholder="Celular"
                 />
@@ -76,6 +142,8 @@ export default function Contacto() {
                   type="email"
                   id="email"
                   name="email"
+                  value={input.email}
+                  onChange={(e) => handlerChange(e)}
                   required
                   placeholder="Correo electrónico"
                 />
@@ -83,13 +151,21 @@ export default function Contacto() {
             </div>
             <textarea
               id="message"
-              name="message"
+              name="mensaje"
+              value={input.mensaje}
+              onChange={(e) => handlerChange(e)}
               required
               placeholder="Contanos sobre tu empresa/negocio y lo que te gustaría conseguir con tu página, nos pondremos en contacto en cuanto podamos."
             ></textarea>
 
             <input type="submit" value="Enviar" className="btnEnviar" />
           </div>
+          <animated.div style={animProps} className="animac">
+            <div className="divAviso">
+              <p>ENVIADO CORRECTAMENTE</p>
+              <img src={enviadoCorr} alt="img" />
+            </div>
+          </animated.div>
         </form>
       </div>
       <div className="imagen">
