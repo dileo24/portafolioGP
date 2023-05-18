b8aa37abc89cf37492487384acc9b5e7b7eb7ca9
Joaquin <joacodileo@gmail.com>
2023-05-18 18:53:30 -0300

    preguntasFrec y figuras en 3D

--- client/src/components/PregFrec.jsx
+++ client/src/components/PregFrec.jsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import imgPreg from "../multimedia/pregFrec.png";
+import ojoAbierto from "../multimedia/ojoAbierto.png";
+import ojoCerrado from "../multimedia/ojoCerrado.png";
 import ButtonExt from "./ButtonExt";
 
 export default function PregFrec() {
+  const preguntas = [
+    {
+      titulo: "PREGUNTA NUMERO UNO",
+      resp: "holaaaaaaaa como va todo bien loco que onda axaoisjdfoajsd.",
+    },
+    {
+      titulo: "PREGUNTA NUMERO DOS",
+      resp: "difjgosdjfs qwue onda papsapspapspapapaaaaaaaaaaaaaaa",
+    },
+    {
+      titulo: "PREGUNTA NUMERO TRES",
+      resp: "holaaaaaaaa como va todo bien loco que onda axaoisjdfoajsd.",
+    },
+    {
+      titulo: "PREGUNTA NUMERO CUATRO",
+      resp: "difjgosdjfs qwue onda papsapspapspapapaaaaaaaaaaaaaaa",
+    },
+    {
+      titulo: "PREGUNTA NUMERO CINCO",
+      resp: "holaaaaaaaa como va todo bien loco que onda axaoisjdfoajsd.",
+    },
+    {
+      titulo: "PREGUNTA NUMERO SEIS",
+      resp: "difjgosdjfs qwue onda papsapspapspapapaaaaaaaaaaaaaaa",
+    },
+  ];
+
   const pagina = useLocation().pathname;
+  const [mostrando, setMostrando] = useState(false);
+
+  const handlePreg = (pregId) => {
+    setMostrando((valorActual) => ({
+      [pregId]: !valorActual[pregId],
+    }));
+  };
 
   return (
     <div className="pregContainer">
@@ -27,60 +63,18 @@ export default function PregFrec() {
         </div>
       </div>
       <div className="pregContainer">
-        <div className="preg">
-          <p className="titulo">PREGUNTA NUMERO UNO</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
-            aspernatur ullam soluta sit, tempore quia sequi nam eius esse enim
-            sapiente non reprehenderit, omnis architecto dolores veniam!
-            Assumenda, doloremque excepturi.
-          </p>
-        </div>
-        <div className="preg">
-          <p className="titulo">PREGUNTA NUMERO DOS</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
-            aspernatur ullam soluta sit, tempore quia sequi nam eius esse enim
-            sapiente non reprehenderit, omnis architecto dolores veniam!
-            Assumenda, doloremque excepturi.
-          </p>
-        </div>
-        <div className="preg">
-          <p className="titulo">PREGUNTA NUMERO TRES</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
-            aspernatur ullam soluta sit, tempore quia sequi nam eius esse enim
-            sapiente non reprehenderit, omnis architecto dolores veniam!
-            Assumenda, doloremque excepturi.
-          </p>
-        </div>
-        <div className="preg">
-          <p className="titulo">PREGUNTA NUMERO CUATRO</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
-            aspernatur ullam soluta sit, tempore quia sequi nam eius esse enim
-            sapiente non reprehenderit, omnis architecto dolores veniam!
-            Assumenda, doloremque excepturi.
-          </p>
-        </div>
-        <div className="preg">
-          <p className="titulo">PREGUNTA NUMERO CINCO</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
-            aspernatur ullam soluta sit, tempore quia sequi nam eius esse enim
-            sapiente non reprehenderit, omnis architecto dolores veniam!
-            Assumenda, doloremque excepturi.
-          </p>
-        </div>
-        <div className="preg">
-          <p className="titulo">PREGUNTA NUMERO SEIS</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
-            aspernatur ullam soluta sit, tempore quia sequi nam eius esse enim
-            sapiente non reprehenderit, omnis architecto dolores veniam!
-            Assumenda, doloremque excepturi.
-          </p>
-        </div>
+        {preguntas.map((preg, pregId) => (
+          <div className="preg">
+            <div className="superior" onClick={() => handlePreg(pregId)}>
+              <p className="titulo">{preg.titulo}</p>
+              <img
+                src={mostrando[pregId] ? ojoAbierto : ojoCerrado}
+                alt="ojo"
+              />
+            </div>
+            {mostrando[pregId] && <p>{preg.resp}</p>}
+          </div>
+        ))}
       </div>
       <ButtonExt pagina={pagina} />
       <Footer />
